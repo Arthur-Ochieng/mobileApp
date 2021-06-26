4045d518f8f24b6ae7d91aee91cc164204ffd237
Arthur-Ochieng <johnarthur016@gmail.com>
2021-06-26 15:55:49 +0300

    initial commit

---
+++ main/models.py
@@ -0,0 +1,47 @@
+# from django.db import models
+
+# class tb1_Authentication(models.Model):
+#     Empcode = models.IntegerField()
+#     username = models.CharField(max_length=50, default='')
+#     password = models.CharField(max_length=50, default='')
+#     ls_active = models.IntegerField(null = True)
+
+#     def __str__(self):
+#         return self.username
+
+#     empAuth_objects = models.Manager()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # class Question(models.Model):
+# #     question_text = models.CharField (max_length=200)
+# #     pub_date = models.DateTimeField ('date published')
+
+# #     def __str__(self):
+# #         return self.question_text
+
+# # class Choice(models.Model):
+# #     question = models.ForeignKey (Question, on_delete = models.CASCADE)
+# #     choice_text = models.CharField (max_length=200)
+# #     votes = models.IntegerField (default = 0)
+
+# #     def __str__(self):
+# #         return self.choice_text
+
+
+
+
+
